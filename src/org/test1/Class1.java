fea4fe75f5382c3ce1aa548cd5defa86ba939ab6
jasajosh95 <jasajosh95@gmail.com>
2020-01-09 13:47:06 +0530

    added a code

---
+++ src/org/test1/Class1.java
@@ -0,0 +1,9 @@
+package org.test1;
+
+public class Class1 {
+public void comid() {
+System.out.println("company ID");
+
+}
+
+}
---
+++ src/org/test1/class2.java
@@ -0,0 +1,8 @@
+package org.test1;
+
+public class class2 extends Class1 {
+public static void main(String[] args) {
+	class2 cls = new  class2();
+	cls.comid();
+}
+}
